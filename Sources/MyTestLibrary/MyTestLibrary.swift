50690bd294782b018078b69f5504847db42e99f3
Mauro Capponi <mauro.capponi@gmail.com>
2021-08-11 22:24:53 -0300

    Added init method

--- Sources/MyTestLibrary/MyTestLibrary.swift
+++ Sources/MyTestLibrary/MyTestLibrary.swift
@@ -1,6 +1,10 @@
 public struct MyTestLibrary {
     var text = "Hello, Test Library swift Pack!"
 
+    public init() {
+        
+    }
+    
     public func salute() -> String {
         return "Hi this is a function from a swift package!!!"
     }
