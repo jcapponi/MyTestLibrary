653dbf8d32d2a8491fcaa31415d3c1860bde7afd
Mauro Capponi <mauro.capponi@gmail.com>
2021-08-11 21:42:35 -0300

    Initial Commit

---
+++ Sources/MyTestLibrary/MyTestLibrary.swift
@@ -0,0 +1,7 @@
+public struct MyTestLibrary {
+    var text = "Hello, Test Library swift Pack!"
+
+    public func salute() -> String {
+        return "Hi this is a function from a swift package!!!"
+    }
+}
